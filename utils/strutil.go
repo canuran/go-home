801336d4e415741fe265edf07c69f028bf62b3d3
caiyouyuan <caiyouyuan@bytedance.com>
2021-01-14 01:33:14 +0800

    重构

--- utils/strutil.go
+++ utils/strutil.go
@@ -34,3 +34,40 @@ func StandardizeRunes(input []rune) []rune {
 func StandardizeString(input string) string {
 	return string(StandardizeRunes([]rune(input)))
 }
+
+// 最小编辑距离
+func MinEditDistance(str1, str2 []rune) int {
+	s1len := len(str1)
+	s2len := len(str2)
+	column := make([]int, len(str1)+1)
+	for y := 1; y <= s1len; y++ {
+		column[y] = y
+	}
+	for x := 1; x <= s2len; x++ {
+		column[0] = x
+		lastKey := x - 1
+		for y := 1; y <= s1len; y++ {
+			oldKey := column[y]
+			var incr int
+			if str1[y-1] != str2[x-1] {
+				incr = 1
+			}
+			column[y] = minimum(column[y]+1, column[y-1]+1, lastKey+incr)
+			lastKey = oldKey
+		}
+	}
+	return column[s1len]
+}
+
+func minimum(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
+		}
+	} else {
+		if b < c {
+			return b
+		}
+	}
+	return c
+}
