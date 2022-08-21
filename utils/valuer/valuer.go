127b5e6e23d89c43b6344231ad083a8e5d6b5316
Administrator <Ewing>
2022-08-21 22:46:33 +0800

    通用日期解析器

--- utils/valuer/valuer.go
+++ utils/valuer/valuer.go
@@ -213,40 +213,109 @@ func SplitBytes(bts []byte, count int) [][]byte {
 }
 
 // Int64Slice 从字符串中提取整数
+// 性能是用正则表达式提取的7倍
 func Int64Slice(input []rune) []int64 {
-	var res []int64
+	var int64s []int64
 	var numPre bool
 	var number int64
-	var negative bool
-	maxIndex := len(input) - 1
+	var preNotNum rune
 	for i, r := range input {
 		if r <= '9' && r >= '0' {
 			number = number*10 + int64(r-'0')
-			if i == maxIndex {
-				if negative {
-					res = append(res, -number)
+			if i == len(input)-1 {
+				if preNotNum == '-' {
+					int64s = append(int64s, -number)
 				} else {
-					res = append(res, number)
+					int64s = append(int64s, number)
 				}
-				negative = false
 			}
 			numPre = true
 		} else {
 			if numPre {
-				if negative {
-					res = append(res, -number)
+				if preNotNum == '-' {
+					int64s = append(int64s, -number)
 				} else {
-					res = append(res, number)
+					int64s = append(int64s, number)
 				}
 				number = 0
 			}
 			numPre = false
-			if r == '-' {
-				negative = true
-			} else {
-				negative = false
+			preNotNum = r
+		}
+	}
+	return int64s
+}
+
+var dateFieldLens = [9]int{4, 2, 2, 2, 2, 2, 9, 2, 2}
+
+// ParseGeneralDate 解析任意字段有序的日期
+// 顺序必须是：年月日时分秒纳秒，兼容ISO8601UTC时区
+// 时间字段可以是任意分隔符或填充位数，示例如下：
+// 2022年8月21日19点18分38秒123456789纳秒
+// 20220821191838.123456789
+// 2022-8-21T19:18:38.123456789+8:30
+// 2022-8-21 19:18:38Z(可以没有纳秒)
+// 2022-8-21(可以没有时间，甚至可以没有日期)
+func ParseGeneralDate(input []rune) time.Time {
+	dateFields := [9]int{1, 1, 1}
+	var fieldNum int
+	var fieldLen int
+	var zoneRune rune
+	var nanoTimes = 1000000000
+
+	// 提取日期字段
+	for _, r := range input {
+		if r <= '9' && r >= '0' {
+			fieldLen++
+			if fieldNum == 6 {
+				nanoTimes /= 10 // 纳秒是小数形式
+			}
+			if fieldLen == 1 { // 覆盖字段
+				dateFields[fieldNum] = int(r - '0')
+			} else if fieldLen == dateFieldLens[fieldNum] { // 字段已满
+				dateFields[fieldNum] = dateFields[fieldNum]*10 + int(r-'0')
+				if fieldNum > 7 {
+					break
+				}
+				fieldNum++
+				fieldLen = 0
+			} else { // 追加字段
+				dateFields[fieldNum] = dateFields[fieldNum]*10 + int(r-'0')
+			}
+		} else {
+			// 当前字段长度大于0则进入下一个字段
+			if fieldLen > 0 {
+				if fieldNum > 7 {
+					break
+				}
+				fieldNum++
+				fieldLen = 0
+			}
+
+			// 处理时区，遵循ISO8601：UTC时区
+			if fieldNum > 5 {
+				if r == 'Z' { // 结束遍历
+					zoneRune = r
+					break
+				}
+				if r == '+' || r == '-' { // 跳转时区字段
+					zoneRune = r
+					fieldNum = 7
+				}
 			}
 		}
 	}
-	return res
+
+	// 计算时区与偏移
+	timeZone := time.Local
+	if zoneRune == 'Z' {
+		timeZone = time.UTC
+	} else if zoneRune == '+' {
+		timeZone = time.FixedZone("", dateFields[7]*3600+dateFields[8]*60)
+	} else if zoneRune == '-' {
+		timeZone = time.FixedZone("", -dateFields[7]*3600-dateFields[8]*60)
+	}
+
+	return time.Date(dateFields[0], time.Month(dateFields[1]), dateFields[2],
+		dateFields[3], dateFields[4], dateFields[5], dateFields[6]*nanoTimes, timeZone)
 }
--- utils/valuer/valuer_test.go
+++ utils/valuer/valuer_test.go
@@ -3,7 +3,9 @@ package valuer
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestInt64ify(t *testing.T) {
@@ -26,4 +28,79 @@ func TestElemOr(t *testing.T) {
 	fmt.Println(ElemOr(strP, ""))
 	fmt.Println(ElemOr(ExistOr(nil, strP), ""))
 	fmt.Println(ElemOr(nil, "123"))
+}
+
+func BenchmarkInt64Slice(b *testing.B) {
+	var str = "-123,abc,12132,4354-2343,23243,ewdsfd,23323,5342"
+	for i := 0; i < b.N; i++ {
+		_ = Int64Slice([]rune(str))
+	}
+}
+
+func TestParseGeneralDate(t *testing.T) {
+	zeroDate := ParseGeneralDate([]rune(""))
+	fmt.Println(zeroDate.Format(time.RFC3339Nano))
+	assert.Equal(t, zeroDate, time.Date(1, 1, 1,
+		0, 0, 0, 0, time.Local))
+
+	now := time.Now().UTC()
+	fmt.Println(now.Format(time.RFC3339Nano))
+	assert.Equal(t, now, ParseGeneralDate([]rune(now.Format(time.RFC3339Nano))))
+
+	dates := []string{
+		"20220821191838.123456789",
+		"2022年8月21日19点18分38秒123456789纳秒",
+		"2022-8-21T19:18Z38.123456789",
+		"Time2022year08month21day19hour18mm38sss123456789SSS",
+		"日期：20220821，时间：19:18:38.123456789",
+	}
+	except := time.Date(2022, 8, 21,
+		19, 18, 38, 123456789, time.Local)
+	for _, date := range dates {
+		result := ParseGeneralDate([]rune(date))
+		fmt.Println(result.Format(time.RFC3339Nano))
+		assert.Equal(t, except, result, date)
+	}
+
+	dates = []string{
+		"20220821191838.123456789Z",
+		"2022-8-21T19:18:38.123456789Z",
+		"日期：20220821，时间：19:18:38.123456789Z",
+	}
+	except = time.Date(2022, 8, 21,
+		19, 18, 38, 123456789, time.UTC)
+	for _, date := range dates {
+		result := ParseGeneralDate([]rune(date))
+		fmt.Println(result.Format(time.RFC3339Nano))
+		assert.Equal(t, except, result, date)
+	}
+
+	utcDate := ParseGeneralDate([]rune("2022-8-21T19:18:38.123456789+8:30"))
+	fmt.Println(utcDate.Format(time.RFC3339Nano))
+	assert.Equal(t, utcDate, time.Date(2022, 8, 21, 19, 18,
+		38, 123456789, time.FixedZone("", 8*3600+30*60)))
+
+	utcDate = ParseGeneralDate([]rune("2022-8-21T19:18:38-8:30"))
+	fmt.Println(utcDate.Format(time.RFC3339Nano))
+	assert.Equal(t, utcDate, time.Date(2022, 8, 21, 19, 18,
+		38, 0, time.FixedZone("", -8*3600-30*60)))
+
+	utcDate = ParseGeneralDate([]rune("2022-8-21T19:18:38Z"))
+	fmt.Println(utcDate.Format(time.RFC3339Nano))
+	assert.Equal(t, utcDate, time.Date(2022, 8, 21, 19, 18,
+		38, 0, time.UTC))
+}
+
+func BenchmarkParseGeneralDate(b *testing.B) {
+	var str = "2022-08-21T19:18:38.123456789Z"
+	for i := 0; i < b.N; i++ {
+		_ = ParseGeneralDate([]rune(str))
+	}
+}
+
+func BenchmarkParseDate(b *testing.B) {
+	var str = "2022-08-21T19:18:38.123456789Z"
+	for i := 0; i < b.N; i++ {
+		_, _ = time.Parse(time.RFC3339Nano, str)
+	}
 }
